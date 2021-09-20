2f96ecf91249a3caea1f436a40887f4af6e4cba4
Vlad Starostin <w1df33r@gmail.com>
2021-09-20 12:30:50 +0700

    Move to newer gulp version

--- gulpfile.js
+++ gulpfile.js
@@ -12,10 +12,6 @@ const sass = require("gulp-sass");
 const autoprefixer = require("gulp-autoprefixer");
 // cleanCss - CSS minimization
 const cleanCSS = require("gulp-clean-css");
-// Imagemin - image minimization and optimization
-const imagemin = require("gulp-imagemin");
-// Pngquant - imagemin utility library for png optimization
-const pngquant = require("imagemin-pngquant");
 // inject - injecting html templates in a html files
 const inject = require("gulp-inject");
 // Rename - rename files
@@ -26,6 +22,12 @@ const sourcemaps = require("gulp-sourcemaps");
 const browserSync = require("browser-sync");
 // Del - delete files and folders
 const del = require("del");
+// Plumber - improve error handling during pipelines
+const plumber = require("gulp-plumber");
+// Imagemin - image compression
+const imagemin = require("gulp-imagemin");
+// imagemin-pngquant - png compression plugin
+const imageminPngquant = require("imagemin-pngquant");
 
 // Yargs - get terminal arguments
 const argv = require("yargs").argv;
@@ -34,13 +36,13 @@ const autoOpen = argv.open || argv.o ? true : false;
 // Config properties
 const config = {
   port: 4100,
-  autoOpen: autoOpen
+  autoOpen: autoOpen,
 };
 
 const paths = {
   styles: {
     src: "src/scss/**/*.scss",
-    dest: "dist/css/"
+    dest: "dist/css/",
   },
   scripts: {
     src: "src/js/**/*.js",
@@ -52,25 +54,26 @@ const paths = {
       "node_modules/waypoints/lib/jquery.waypoints.min.js",
       "node_modules/dom-i18n/dist/dom-i18n.min.js",
       "node_modules/howler/dist/howler.min.js",
-      "node_modules/progressively/dist/progressively.min.js"
-    ]
+      "node_modules/progressively/dist/progressively.min.js",
+    ],
   },
   html: {
     contentSrc: "src/html/content/**/*.html",
     includeHeaderSrc: "src/html/includes/header.html",
     includeFooterSrc: "src/html/includes/footer.html",
-    dest: "dist"
+    dest: "dist",
   },
   assets: {
     fontsSrc: "src/fonts/**",
     fontsDest: "dist/fonts",
-    soundsSrc: "src/sounds/**",
-    soundsDest: "dist/sounds",
-    imagesSrc: "src/images/**",
-    imagesDest: "dist/images",
-    projectsSrc: "src/projects/**",
-    projectsDest: "dist/projects"
-  }
+    imgSrc: "src/images/**",
+    assetsSrc: "src/assets/**",
+    assetsDest: "dist/assets",
+  },
+  libraries: {
+    src: "src/libs/**",
+    dest: "dist/libs",
+  },
 };
 
 // Delete dist folder
@@ -81,35 +84,35 @@ function clean(done) {
 }
 
 // Build css from scss, minimize, move to dist and inject if browserSync running
-function styles() {
+function buildStyles() {
   // Returns stream
   return (
     gulp
       .src(paths.styles.src)
+      .pipe(plumber())
       .pipe(sourcemaps.init())
       .pipe(
         sass({
           includePaths: ["node_modules"],
           outputStyle: "compressed",
-          errLogToConsole: true
-        })
+          errLogToConsole: true,
+        }),
       )
-      .on("error", function(err) {
+      .on("error", function (err) {
         console.log(err.toString());
-
         this.emit("end");
       })
       .pipe(
         autoprefixer({
-          cascade: false
-        })
+          cascade: false,
+        }),
       )
       .pipe(cleanCSS())
       // pass in options to the stream
       .pipe(
         rename({
-          suffix: ".min"
-        })
+          suffix: ".min",
+        }),
       )
       .pipe(sourcemaps.write("./")) // path related to target destination
       .pipe(gulp.dest(paths.styles.dest))
@@ -119,29 +122,34 @@ function styles() {
 }
 
 // Build html, inject templates and move to dist
-function html() {
+function buildHtml() {
   // Returns stream
-  return gulp
-    .src(paths.html.contentSrc)
-    .pipe(
-      inject(gulp.src([paths.html.includeHeaderSrc]), {
-        starttag: "<!-- inject:header:{{ext}} -->",
-        transform: function(filePath, file) {
-          // return file contents as string
-          return file.contents.toString("utf8");
-        }
-      })
-    )
-    .pipe(
-      inject(gulp.src([paths.html.includeFooterSrc]), {
-        starttag: "<!-- inject:footer:{{ext}} -->",
-        transform: function(filePath, file) {
-          // return file contents as string
-          return file.contents.toString("utf8");
-        }
-      })
-    )
-    .pipe(gulp.dest(paths.html.dest));
+  return (
+    gulp
+      .src(paths.html.contentSrc)
+      .pipe(plumber())
+      .pipe(
+        inject(gulp.src([paths.html.includeHeaderSrc]), {
+          starttag: "<!-- inject:header:{{ext}} -->",
+          transform: function (filePath, file) {
+            // return file contents as string
+            return file.contents.toString("utf8");
+          },
+        }),
+      )
+      .pipe(
+        inject(gulp.src([paths.html.includeFooterSrc]), {
+          starttag: "<!-- inject:footer:{{ext}} -->",
+          transform: function (filePath, file) {
+            // return file contents as string
+            return file.contents.toString("utf8");
+          },
+        }),
+      )
+      .pipe(gulp.dest(paths.html.dest))
+      // Inject css if browserSync running
+      .pipe(browserSync.stream())
+  );
 }
 
 // Build, concat, minimize and move to dist third party libraries
@@ -149,131 +157,138 @@ function tplScripts() {
   // Returns stream
   return gulp
     .src(paths.scripts.tpl)
-    .pipe(concat("components.min.js")) // Собираем их в кучу в новом файле components.min.js
-    .pipe(uglify()) // Сжимаем JS файл
-    .pipe(gulp.dest(paths.scripts.dest)); // Выгружаем в папку app/js
+    .pipe(plumber())
+    .pipe(concat("components.min.js")) // Take all of components and compile into components.min.js
+    .pipe(uglify()) // Minify this file
+    .pipe(gulp.dest(paths.scripts.dest)); // Move it to dist/js
 }
 // Build, minimize, and move to dist custom scripts
 function customScripts() {
   // Returns stream
   return gulp
     .src(paths.scripts.src)
+    .pipe(plumber())
     .pipe(sourcemaps.init())
     .pipe(
       babel({
-        presets: ["@babel/env"]
-      })
+        presets: ["@babel/env"],
+      }),
     )
-    .pipe(uglify()) // Сжимаем JS файл
+    .pipe(uglify()) // Minify this file
     .pipe(
       rename({
-        suffix: ".min"
-      })
+        suffix: ".min", // Rename it
+      }),
     )
     .pipe(sourcemaps.write("./")) // path related to target destination
-    .pipe(gulp.dest(paths.scripts.dest)); // Выгружаем в папку app/js
+    .pipe(gulp.dest(paths.scripts.dest)); // Move it to dist/js
 }
-const scripts = gulp.parallel(customScripts, tplScripts);
+const buildScripts = gulp.parallel(customScripts, tplScripts);
 
 // Move fonts from working to dist directory
-function fonts() {
+function buildFonts() {
   return gulp
     .src(paths.assets.fontsSrc)
     .pipe(gulp.dest(paths.assets.fontsDest));
 }
+
 // Optimize and move images from working to dist directory
-function cleanImgFolder(done) {
-  del.sync("dist/images");
-  done();
+function buildImagesProd() {
+  return (
+    gulp
+      .src(paths.assets.imgSrc) // Take all iamages
+      .pipe(plumber())
+      // Optimize
+      .pipe(
+        imagemin([
+          imageminPngquant({
+            quality: [0.7, 0.9], // Image quality around 70%-90%
+            speed: 1, // The lowest speed of optimization with the highest quality
+            floyd: 1, // Controls level of dithering (0 = none, 1 = full).
+          }),
+        ]),
+      )
+      .pipe(gulp.dest(paths.assets.assetsDest))
+  ); // Move it to dist/assets
 }
-function images() {
-  return gulp
-    .src(paths.assets.imagesSrc) // Берем все изображения из app
-    .pipe(
-      imagemin({
-        // Сжимаем их с наилучшими настройками с учетом кеширования
-        interlaced: true,
-        progressive: true,
-        svgoPlugins: [{ removeViewBox: false }],
-        use: [pngquant()]
-      })
-    )
-    .pipe(gulp.dest(paths.assets.imagesDest));
-}
-
-function sounds() {
-  return gulp
-    .src(paths.assets.soundsSrc)
-    .pipe(gulp.dest(paths.assets.soundsDest));
+function buildImagesDev() {
+  // Move images without optimization to save rebuild time on dev
+  return gulp.src(paths.assets.imgSrc).pipe(gulp.dest(paths.assets.assetsDest));
 }
 
-function projects() {
+function buildAssets() {
+  // Move other assets / non-images
   return gulp
-    .src(paths.assets.projectsSrc)
-    .pipe(gulp.dest(paths.assets.projectsDest));
+    .src(paths.assets.assetsSrc)
+    .pipe(gulp.dest(paths.assets.assetsDest));
 }
 
-const assets = gulp.parallel(fonts, images, sounds, projects);
+// Export all libraries
+function buildLibraries() {
+  // Returns stream
+  return gulp.src(paths.libraries.src).pipe(gulp.dest(paths.libraries.dest)); // Move it to dist/libs
+}
+
+const buildAssetsProd = gulp.parallel(buildFonts, buildImagesProd, buildAssets);
+const buildAssetsDev = gulp.parallel(buildFonts, buildImagesDev, buildAssets);
 
 // Config and start browser sync
 function bSync() {
   browserSync({
-    // Выполняем browser Sync
+    // Run browser Sync
     server: {
-      baseDir: "dist"
+      baseDir: "dist",
     },
     port: config.port,
-    notify: false, // Отключаем уведомления
-    open: false // Авто открытие в браузере
+    notify: false, // Notifications off
+    open: false, // Auto open off
   });
 }
 
 // Start file watch
 function watchFiles() {
   // Inject styles on change, styles injection handled in a styles task
-  gulp.watch(paths.styles.src, styles);
+  gulp.watch(paths.styles.src, buildStyles);
+  // Build html and reload, it's possible to inject but not recommended
+  gulp.watch(
+    [
+      paths.html.contentSrc,
+      paths.html.includeFooterSrc,
+      paths.html.includeHeaderSrc,
+    ],
+    buildHtml,
+  );
+
   // Build scripts and reload on change, it's possible to inject but not recommended
   gulp
-    .watch(paths.scripts.src, gulp.series(scripts))
+    .watch(paths.scripts.src, gulp.series(buildScripts))
     .on("change", browserSync.reload);
-  // Build html and reload, it's possible to inject but not recommended
+  // Reload on libs change
   gulp
-    .watch(
-      [
-        paths.html.contentSrc,
-        paths.html.includeFooterSrc,
-        paths.html.includeHeaderSrc
-      ],
-      gulp.series(html)
-    )
+    .watch(paths.libraries.src, gulp.series(buildLibraries))
     .on("change", browserSync.reload);
-  // Reload on images change
+  // Reload on assets change
   gulp
-    .watch(paths.assets.imagesSrc, gulp.series(cleanImgFolder, images))
+    .watch(paths.assets.assetsSrc, gulp.series(buildAssetsDev))
+    .on("change", browserSync.reload);
+  gulp
+    .watch(paths.assets.imgSrc, gulp.series(buildAssetsDev))
     .on("change", browserSync.reload);
   // Reload on fonts change
   gulp
-    .watch(paths.assets.fontsSrc, gulp.series(fonts))
-    .on("change", browserSync.reload);
-
-  // Reload on sounds change
-  gulp
-    .watch(paths.assets.soundsSrc, gulp.series(sounds))
-    .on("change", browserSync.reload);
-
-  // Reload on projects change
-  gulp
-    .watch(paths.assets.projectsSrc, gulp.series(projects))
+    .watch(paths.assets.fontsSrc, gulp.series(buildFonts))
     .on("change", browserSync.reload);
 }
 
-exports.html = html;
-exports.styles = styles;
-exports.scripts = scripts;
-exports.assets = assets;
-
 // To chain series funtions must return callback, promise, stream, observable, child process
-const build = gulp.series(clean, styles, html, scripts, assets);
+const build = gulp.series(
+  clean,
+  buildStyles,
+  buildHtml,
+  buildScripts,
+  buildAssetsProd,
+  buildLibraries,
+);
 exports.build = build;
 
 const watch = gulp.series(build, gulp.parallel(watchFiles, bSync));
